f8dda4d431f0a4e3852e24562ef47e4cf3e5add7
Victoria Johns <vmichellej@gmail.com>
2016-05-22 22:28:30 -0400

    Delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,12 +28,19 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
   end
 
   private
